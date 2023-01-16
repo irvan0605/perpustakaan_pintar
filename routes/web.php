621218a3fd0e3196b3949d01357b8c3b95baba55
Irvan Maulana <irvanmaulana0605@gmail.com>
2023-01-16 16:54:23 +0700

    update: bg

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
+Route::get('/', function () {
+    return redirect('/login');
+});
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/profile', 'ProfileController@index')->name('profile');
